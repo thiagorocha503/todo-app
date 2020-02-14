484d662fc04f5b63642c739f95ad4f67d15be6ad
Thiago Rocha <thiagorocha503@gmail.com>
2020-02-14 17:35:42 -0300

    :globe_with_meridians:Adiciona internacionalização aos widget
    Nesse commit foi adiciona as opções de idioma português, inglês e espanhol aos widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:lista_de_tarefa/view/tarefaListPage.dart';
 
-
-
-void main(){
+void main() {
   runApp(MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        
       ),
-     debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      supportedLocales: [const Locale('en', 'US'),
+                         const Locale('pt', 'BR'),
+                         const Locale('es')],                
+      debugShowCheckedModeBanner: false,
       home: TarefaListPage()
-    )
-  );
+      )
+    );
 }
