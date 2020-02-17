5f1903d382dc672ae88afa9dd942c354b7b6caed
Thiago <thiagorocha503@gmail.com>
2020-02-16 21:34:43 -0300

    :bug:Corrige importação de pacote

--- lib/view/tarefaNewPage.dart
+++ lib/view/tarefaNewPage.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:lista_de_tarefa/presenter/presenter.dart';
 import 'package:lista_de_tarefa/presenter/tarefaAddPresenter.dart';
-import 'package:lista_de_tarefa/util/dateConversion.dart';
+import 'package:lista_de_tarefa/util/DateConversion.dart';
+//import 'package:lista_de_tarefa/util/dateConversion.dart';
 import 'package:lista_de_tarefa/util/validation.dart';
 import 'package:lista_de_tarefa/view/view.dart';
 
