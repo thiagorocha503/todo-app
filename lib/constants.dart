e02a93864bbd3d60d8260bbeb085a0040570b0cc
Thiago Rocha <thiagorocha503@gmail.com>
2020-06-30 15:23:25 -0300

    ✨Update contact email

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
-const EMAIL_DEV = "thiagorocha503@gmail.com";
+const EMAIL_DEV = "thiagorocha.dev@gmail.com";
 const String ANDROID_APP_LINK = "https://play.google.com/store";
 const String ANDROID_APP_ID = "";
 const String version = "v1.0.0";
