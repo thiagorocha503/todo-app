0c4f6df09e0c0ba09820125116b9ad013089b34c
Thiago Rocha Ferreira <thiagorocha503@gmail.com>
2022-12-19 20:42:51 -0300

    ✅ Refact test

--- test/todo/todo_over_view_test.dart
+++ test/todo/todo_over_view_test.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:todo/filter/model/filter.dart';
 import 'package:todo/todo_over_view/bloc/todo_over_view_bloc.dart';
 import 'package:todo/todo_over_view/bloc/todo_over_view_event.dart';
@@ -11,183 +10,213 @@ import '../mock/todo_repository_mok.dart';
 
 void main() {
   late Bloc bloc;
-  group("Todo", () {
-    setUp(() {
-      bloc = TodoOverViewBloc(
-          filter: Filter.all, repository: TodoRepositoryMock());
-    });
-    blocTest(
-      "Get ",
-      build: () => bloc,
-      act: (bloc) => bloc.add(
-        TodoOverViewFetchEvent(),
-      ),
-      expect: () => [
-        TodoOverViewLoadingState(),
-        TodoOverViewLoadedState(todos: [
-          Todo(
-            id: 1,
-            name: "delectus aut autem",
-            createdDate: DateTime(DateTime.now().year, 1, 1),
-          ),
-          Todo(
-            id: 2,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-            completeDate: DateTime(DateTime.now().year, 3, 15),
-          ),
-          Todo(
-            id: 3,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-          ),
-        ])
-      ],
-    );
 
-    blocTest(
-      "delete ",
-      build: () => bloc,
-      act: (bloc) => bloc.add(
-        TodoOverViewDeleted(id: 2),
-      ),
-      expect: () => [
-        TodoOverViewLoadingState(),
-        TodoOverViewLoadedState(todos: [
-          Todo(
-            id: 1,
-            name: "delectus aut autem",
-            createdDate: DateTime(DateTime.now().year, 1, 1),
-          ),
-          Todo(
-            id: 3,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-          ),
-        ])
-      ],
-    );
+  final List<Todo> todos = [
+    Todo(
+      id: 1,
+      name: "delectus aut autem",
+      createdDate: DateTime(DateTime.now().year, 1, 1),
+      completeDate: null,
+      dueDate: null,
+    ),
+    Todo(
+      id: 2,
+      name: "quis ut nam facilis et officia qui",
+      createdDate: DateTime(DateTime.now().year, 2, 1),
+      completeDate: DateTime(DateTime.now().year, 3, 15),
+      dueDate: null,
+    ),
+    Todo(
+      id: 3,
+      name: "quis ut nam facilis et officia qui",
+      createdDate: DateTime(DateTime.now().year, 2, 1),
+      dueDate: null,
+    ),
+  ];
 
-    blocTest(
-      "insert ",
-      build: () => bloc,
-      act: (bloc) => bloc.add(
-        TodoOverViewAdded(
-          todo: Todo(
-            name:
-                "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            createdDate: DateTime(
-              DateTime.now().year,
-              6,
-              20,
-            ),
-          ),
+  void setUp() {
+    bloc = TodoOverViewBloc(
+      filter: Filter.all,
+      repository: MockTodoRepository(todos: todos),
+    );
+  }
+
+  blocTest(
+    "emits TodoOverViewLoadingState and TodoOverViewLoadedState "
+    "when TodoOverViewFetchEvent is added.",
+    setUp: setUp,
+    build: () => bloc,
+    act: (bloc) => bloc.add(
+      TodoOverViewFetchEvent(),
+    ),
+    expect: () => [
+      TodoOverViewLoadingState(),
+      TodoOverViewLoadedState(todos: [
+        Todo(
+          id: 1,
+          name: "delectus aut autem",
+          createdDate: DateTime(DateTime.now().year, 1, 1),
+        ),
+        Todo(
+          id: 2,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+          completeDate: DateTime(DateTime.now().year, 3, 15),
+        ),
+        Todo(
+          id: 3,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+        ),
+      ])
+    ],
+  );
+
+  blocTest(
+    "emits TodoOverViewLoadingState and TodoOverViewLoadedState "
+    "when TodoOverViewDeleted is added.",
+    setUp: setUp,
+    build: () => bloc,
+    act: (bloc) => bloc.add(
+      TodoOverViewDeleted(id: 2),
+    ),
+    expect: () => [
+      TodoOverViewLoadingState(),
+      TodoOverViewLoadedState(todos: [
+        Todo(
+          id: 1,
+          name: "delectus aut autem",
+          createdDate: DateTime(DateTime.now().year, 1, 1),
+        ),
+        Todo(
+          id: 3,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+        ),
+      ])
+    ],
+  );
+
+  blocTest(
+    "emits TodoOverViewLoadingState and TodoOverViewLoadedState "
+    "when TodoOverViewAdded is added.",
+    setUp: setUp,
+    build: () => bloc,
+    act: (bloc) => bloc.add(
+      TodoOverViewAdded(
+        todo: Todo(
+          id: 4,
+          name:
+              "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          createdDate: DateTime(DateTime.now().year, 6, 20),
         ),
       ),
-      expect: () => [
-        TodoOverViewLoadingState(),
-        TodoOverViewLoadedState(todos: [
-          Todo(
-            id: 1,
-            name: "delectus aut autem",
-            createdDate: DateTime(DateTime.now().year, 1, 1),
-          ),
-          Todo(
-            id: 2,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-            completeDate: DateTime(DateTime.now().year, 3, 15),
-          ),
-          Todo(
-            id: 3,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-          ),
-          Todo(
-            id: 4,
-            name:
-                "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            createdDate: DateTime(
-              DateTime.now().year,
-              6,
-              20,
-            ),
-          ),
-        ])
-      ],
-    );
+    ),
+    expect: () => [
+      TodoOverViewLoadingState(),
+      TodoOverViewLoadedState(todos: [
+        Todo(
+          id: 1,
+          name: "delectus aut autem",
+          createdDate: DateTime(DateTime.now().year, 1, 1),
+          completeDate: null,
+          dueDate: null,
+        ),
+        Todo(
+          id: 2,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+          completeDate: DateTime(DateTime.now().year, 3, 15),
+          dueDate: null,
+        ),
+        Todo(
+          id: 3,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+          dueDate: null,
+        ),
+        Todo(
+          id: 4,
+          name:
+              "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          createdDate: DateTime(DateTime.now().year, 6, 20),
+        ),
+      ])
+    ],
+  );
 
-    blocTest(
-      "Todo checked",
-      build: () => bloc,
-      act: (bloc) => bloc.add(
-        TodoOverViewUpdate(
-          todo: Todo(
-            id: 1,
-            name: "delectus aut autem",
-            createdDate: DateTime(DateTime.now().year, 1, 1),
-            completeDate: DateTime(DateTime.now().year, 2, 20),
-          ),
+  blocTest(
+    "emits TodoOverViewLoadedState when TodoOverViewUpdate check is added.",
+    setUp: setUp,
+    build: () => bloc,
+    act: (bloc) => bloc.add(
+      TodoOverViewUpdate(
+        todo: Todo(
+          id: 1,
+          name: "delectus aut autem",
+          createdDate: DateTime(DateTime.now().year, 1, 1),
+          completeDate: DateTime(DateTime.now().year, 2, 20),
         ),
       ),
-      expect: () => [
-        TodoOverViewLoadedState(todos: [
-          Todo(
-            id: 1,
-            name: "delectus aut autem",
-            createdDate: DateTime(DateTime.now().year, 1, 1),
-            completeDate: DateTime(DateTime.now().year, 2, 20),
-          ),
-          Todo(
-            id: 2,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-            completeDate: DateTime(DateTime.now().year, 3, 15),
-            dueDate: null,
-          ),
-          Todo(
-            id: 3,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-          ),
-        ])
-      ],
-    );
+    ),
+    expect: () => [
+      TodoOverViewLoadedState(todos: [
+        Todo(
+          id: 1,
+          name: "delectus aut autem",
+          createdDate: DateTime(DateTime.now().year, 1, 1),
+          completeDate: DateTime(DateTime.now().year, 2, 20),
+        ),
+        Todo(
+          id: 2,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+          completeDate: DateTime(DateTime.now().year, 3, 15),
+          dueDate: null,
+        ),
+        Todo(
+          id: 3,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+        ),
+      ])
+    ],
+  );
 
-    blocTest(
-      "Todo unchecked",
-      build: () => bloc,
-      act: (bloc) => bloc.add(
-        TodoOverViewUpdate(
-          todo: Todo(
-            id: 2,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-            completeDate: null,
-          ),
+  blocTest(
+    "Todo TodoOverViewLoadedState when TodoOverViewUpdate unchecked is added.",
+    setUp: setUp,
+    build: () => bloc,
+    act: (bloc) => bloc.add(
+      TodoOverViewUpdate(
+        todo: Todo(
+          id: 2,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+          completeDate: null,
         ),
       ),
-      expect: () => [
-        TodoOverViewLoadedState(todos: [
-          Todo(
-            id: 1,
-            name: "delectus aut autem",
-            createdDate: DateTime(DateTime.now().year, 1, 1),
-            completeDate: null,
-          ),
-          Todo(
-            id: 2,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-            completeDate: null,
-          ),
-          Todo(
-            id: 3,
-            name: "quis ut nam facilis et officia qui",
-            createdDate: DateTime(DateTime.now().year, 2, 1),
-          ),
-        ])
-      ],
-    );
-  });
+    ),
+    expect: () => [
+      TodoOverViewLoadedState(todos: [
+        Todo(
+          id: 1,
+          name: "delectus aut autem",
+          createdDate: DateTime(DateTime.now().year, 1, 1),
+          completeDate: null,
+        ),
+        Todo(
+          id: 2,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+          completeDate: null,
+        ),
+        Todo(
+          id: 3,
+          name: "quis ut nam facilis et officia qui",
+          createdDate: DateTime(DateTime.now().year, 2, 1),
+        ),
+      ])
+    ],
+  );
 }
