534c6f81f6522502cbb410b6a696a41b1507f66a
Thiago Ferreira <thiagorocha503@gmail.com>
2023-05-01 12:29:45 -0300

    ⬆️ Upgrade packages

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:todo/constants/database.dart';
-// ignore: depend_on_referenced_packages
 import 'package:path/path.dart';
 import 'package:flutter/foundation.dart';
 import 'package:todo/database/schema.dart';
