6b2ac48dac49494f61170dfa603247318359c64d
Thiago Ferreira <thiagorocha503@gmail.com>
2025-08-08 18:35:21 -0300

    fix: add return type void to AppToast.showToast method

--- lib/shared/widget/toast.dart
+++ lib/shared/widget/toast.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class AppToast {
-  static showToast(String message) {
+  static void showToast(String message) {
     Fluttertoast.showToast(
       msg: message,
       toastLength: Toast.LENGTH_SHORT,
